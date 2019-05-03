9651bb0835d90584ef235ede56d16dc2e6bc6f6c
kongbai0920 <1052042911@qq.com>
2019-05-03 11:39:08 +0800

    add run

---
+++ homework/api_test/run.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+
+import unittest
+from api_test.Public import test_path
+from api_test.test_case.register_case import TestRegister
+from api_test.test_case.recharge_case import TestRecharge
+from api_test.test_case.login_case import TestLogin
+from api_test.test_case.invest_case import TestInvest
+from api_test.test_case.add_case import TestAdd
+from api_test.Public import test_path
+import HTMLTestRunnerNew
+
+
+suite=unittest.TestSuite()
+
+loader=unittest.TestLoader()
+
+discover=unittest.defaultTestLoader.discover(test_path.case_path,'*_case.py')
+
+# suite.addTest(loader.loadTestsFromTestCase(TestInvest))
+
+
+with open(test_path.report_path,mode='wb+') as file:
+    runner=HTMLTestRunnerNew.HTMLTestRunner(stream=file, verbosity=2,title='接口测试',description=None,tester='KB')
+    runner.run(discover)
