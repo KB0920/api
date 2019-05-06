f6a84a324e1a3e63d81be5e1e0bf58428eeae7ca
kongbai0920 <1052042911@qq.com>
2019-05-06 21:18:26 +0800

    update run

--- homework/webservice/run.py
+++ homework/webservice/run.py
@@ -5,9 +5,9 @@ from webservice.Common import path
 import sys
 
 sys.path.append('./')
-print(sys.path)
-# discover=unittest.defaultTestLoader.discover(path.case_path,'*_case.py')
-#
-# with open(path.report_path,'wb+') as file:
-#     runner=HTMLTestRunnerNew.HTMLTestRunner(stream=file, verbosity=2,title='webservice测试',description=None,tester='KB')
-#     runner.run(discover)
+
+discover=unittest.defaultTestLoader.discover(path.case_path,'*_case.py')
+
+with open(path.report_path,'wb+') as file:
+    runner=HTMLTestRunnerNew.HTMLTestRunner(stream=file, verbosity=2,title='webservice测试',description=None,tester='KB')
+    runner.run(discover)
