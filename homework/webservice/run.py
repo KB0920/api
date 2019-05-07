485d17f39e44220c7811d72bd425fb3c5cdba3c8
kongbai0920 <1052042911@qq.com>
2019-05-07 11:49:53 +0800

    update run

--- homework/webservice/run.py
+++ homework/webservice/run.py
@@ -1,9 +1,16 @@
 # -*- coding:utf-8 -*-
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+
+
 import HTMLTestRunnerNew
 import unittest
 from webservice.Common import path
-import sys
-sys.path.append('./.')
+
 
 discover=unittest.defaultTestLoader.discover(path.case_path,'*_case.py')
 
